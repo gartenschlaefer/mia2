e11e1924785b98fdbc73cb75440c8b8108600538
Chris <christian.walter@outlook.at>
2020-03-20 22:38:26 +0100

    sdm embeddings done

--- e2_main_structure-analysis.py
+++ e2_main_structure-analysis.py
@@ -14,12 +14,63 @@ from scipy import stats
 from scipy.cluster import hierarchy
 
 
+def plot_sdm(sdm, vmin=None, vmax=None, cmap='magma', plot_dir=None, emb=None):
+  """
+  plot sdm matrix (simple imshow)
+  """
+  plt.figure()
+  plt.imshow(sdm[::-1, :], aspect='auto', vmin=vmin, vmax=vmax, cmap=cmap, extent=[0, len(sdm), 0, len(sdm)])
+  plt.ylabel('frames')
+  plt.xlabel('frames')
+  plt.colorbar()
+  plt.tight_layout()
+
+  if plot_dir is not None:
+    plt.savefig(plot_dir + 'sdm_emb-'+ str(emb) + '.png', dpi=150)
+
+  else:
+    plt.show()
+
+
 # --
 # Main function
 if __name__ == '__main__':
 
   # read audiofile
   file_dir = './ignore/e2_data/'
+  plot_dir = './ignore/e2_data/plots/'
+
+  # file names
+  file_name_data = 'seq3.mat'
+
+  # load file
+  data = loadmat(file_dir + file_name_data)
+
+
+  # --
+  # extract features
+
+  # chroma features
+  chroma = data['CHROM']
+  print("chroma: ", chroma.shape)
+
+
+  # --
+  # SDM normal
+
+  sdm_chroma = calc_sdm(chroma)
+  print("sdm: ", sdm_chroma.shape)
+  plot_sdm(sdm_chroma, plot_dir=plot_dir)
+
+
+  # --
+  # Embedding Dimensions
+
+  for emb in range(1, 17):
+    sdm_chroma_emb = calc_sdm(chroma, emb=emb)
+    print("sdm emb: ", sdm_chroma_emb.shape)
+    plot_sdm(sdm_chroma_emb, plot_dir=plot_dir, emb=emb)
+
 
 
 
--- mia2.py
+++ mia2.py
@@ -3,6 +3,40 @@
 
 import numpy as np
 
+
+def calc_sdm(feat_frames, distance_measure='euclidean', emb=None):
+  """
+  calculate the self-distance matrix from frame feature vectors [n x m] i.e. [features x frames]
+  """
+
+  # init
+  n, m = feat_frames.shape
+  sdm = np.zeros((m, m))
+
+  # run through each feature
+  for i, feat in enumerate(feat_frames.T):
+
+    # compare with each other feature frame
+    for j in range(m):
+      
+      # check embedding
+      if emb is None:
+
+        # calculate distance
+        sdm[i, j] = np.linalg.norm(feat - feat_frames[:, j])
+
+      # do the embedding stuff
+      else:
+
+        # extend feat_frames with zeros
+        e = np.hstack((feat_frames.copy(), np.zeros((n, emb+1))))
+
+        # embedding implementation
+        sdm[i, j] = np.linalg.norm(e[:, i:i+emb+1] - e[:, j:j+emb+1])
+
+  return sdm
+
+
 def calc_pca(x):
   """
   calculate pca of signal, already ordered, n x m (samples x features)

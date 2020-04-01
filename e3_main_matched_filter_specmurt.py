6eb1e674c54b861fbb7a7fdd3b1fccc495faae3b
Nico Seddiki <nico.seddiki@gmail.com>
2020-04-01 14:54:47 +0200

    Added non-lin mapping function

--- e3_main_matched_filter_specmurt.py
+++ e3_main_matched_filter_specmurt.py
@@ -3,12 +3,19 @@ import scipy as sci
 import matplotlib.pyplot as plt
 
 # my personal mia lib
-from mia2 import *
+from mia2 import non_linear_mapping
 
 # Librosa module for calculating the Constant-Q Transform
-import librosa.core.constantq as qt
+import librosa as libr
 
 #------------------------------------------------------------------------------
 # Main function
 if __name__ == '__main__':
+    
+    # Loading file in memory 
+    file_name = 'Cmaj7_9.wav'
+    file_path = 'ignore/sounds/'
+    full_name = file_path + file_name
+     
+    audio_data, sampling_rate = libr.load( full_name, sr=None, duration=0.5 )
     non_linear_mapping( )

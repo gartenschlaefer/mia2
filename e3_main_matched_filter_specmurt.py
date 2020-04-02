b9d16b3d1577a8abda0a2a8a065594135aa85f4e
Nico Seddiki <nico.seddiki@gmail.com>
2020-04-02 16:59:49 +0200

    Plot functions implemented

--- e3_main_matched_filter_specmurt.py
+++ e3_main_matched_filter_specmurt.py
@@ -17,6 +17,14 @@ def plot_CQT_spectrum( cqt_spectrum ):
     plt.colorbar(format='%+2.0f dB')
     plt.title('Constant-Q power spectrum')
     plt.tight_layout()
+    plt.show()  
+
+#------------------------------------------------------------------------------
+def plot_harmonic_structure( common_harmonic_structure ):
+    plt.stem( common_harmonic_structure, linefmt=None, markerfmt=None, 
+        basefmt=None, use_line_collection=True)
+    plt.xlabel( 'Log-freq. bin number' )
+    plt.ylabel( 'Relative amplitude of harmonic component' )
     plt.show()
 
 #------------------------------------------------------------------------------
@@ -38,7 +46,7 @@ def initial_harmonics( list_hormonics,
     else:
         print( "No other options available!" )
                    
-    return common_harmonic_structure    
+    return common_harmonic_structure  
 
 #------------------------------------------------------------------------------
 # Main function
@@ -51,20 +59,27 @@ if __name__ == '__main__':
      
     audio_data, sampling_rate = libr.load( full_name, sr=None )
 
-    # Compute and plot CQT
+    # Compute and plot CQT-----------------------------------------------------
     # - One frequency bin has a length of 1379 (for Cmaj.wav)
     # - 48 bins in total -> 48 times 1379 
     cqt_spectrum = libr.cqt( audio_data, sr=sampling_rate, fmin=110, 
         n_bins=24, bins_per_octave=12 )
-    plot_CQT_spectrum( cqt_spectrum )
-    
-    # Define common harmonic structure
+   
+    # Define common harmonic structure-----------------------------------------
     # - number of frequency bins is the same as for the cqt -> n_bins = 48
     list_hormonics = [0, 12, 19, 24, 28, 31]
     common_harmonic_structure = np.zeros(( 48, 1 ))
 
     common_harmonic_structure = initial_harmonics( list_hormonics, 
-        common_harmonic_structure, option=1 )
+        common_harmonic_structure, option=2 )
+    
+    # Plots so far-------------------------------------------------------------
+    # plot_CQT_spectrum( cqt_spectrum )
+    # plot_harmonic_structure( common_harmonic_structure )
+    
+    # Initial guess for fundamental frequency distribution---------------------
+    # - Done via inverse filter approach.
 
-    #--------------------------------------------------------------------------
+
+    # Non-linear mapping function----------------------------------------------
     non_linear_mapping( )

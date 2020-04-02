3a9db61e671447118010e93566f129428853b97a
Nico Seddiki <nico.seddiki@gmail.com>
2020-04-02 14:17:14 +0200

    CQT computed

--- e3_main_matched_filter_specmurt.py
+++ e3_main_matched_filter_specmurt.py
@@ -7,6 +7,17 @@ from mia2 import non_linear_mapping
 
 # Librosa module for calculating the Constant-Q Transform
 import librosa as libr
+from librosa.display import specshow
+
+#------------------------------------------------------------------------------
+def plot_CQT_spectrum( cqt_spectrum ):
+    specshow( libr.amplitude_to_db( np.abs(cqt_spectrum), ref=np.max),
+        sr=sampling_rate, x_axis='time', y_axis='cqt_note')
+    
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Constant-Q power spectrum')
+    plt.tight_layout()
+    plt.show()
 
 #------------------------------------------------------------------------------
 # Main function
@@ -17,5 +28,13 @@ if __name__ == '__main__':
     file_path = 'ignore/sounds/'
     full_name = file_path + file_name
      
-    audio_data, sampling_rate = libr.load( full_name, sr=None, duration=0.5 )
+    audio_data, sampling_rate = libr.load( full_name, sr=None )
+
+    # Compute and plot CQT
+    cqt_spectrum = libr.cqt( audio_data, sr=sampling_rate, fmin=50, 
+        n_bins=120, bins_per_octave=24 )
+    
+    plot_CQT_spectrum( cqt_spectrum )
+    
+    #--------------------------------------------------------------------------
     non_linear_mapping( )

7ce4b16e00cdbad63e28dadc25cb5ef12e0e740f
Nico Seddiki <nico.seddiki@gmail.com>
2020-06-28 20:37:28 +0200

    Added MPL class to git

---
+++ MLP.py
@@ -0,0 +1,16 @@
+#---------------------------------------------------------------
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#---------------------------------------------------------------
+class MLP_Net( nn.Module ):
+    def __init__( self, in_dim, hid_dim, out_dim ):
+        super( MLP_Net, self ).__init__(  )
+        self.fc1 = nn.Linear( in_dim, hid_dim )
+        self.fc2 = nn.Linear( hid_dim, out_dim )
+    
+    def forward( self, x ):
+        x = F.relu( self.fc1( x ) )
+        x = F.sigmoid( self.fc2( x ) )
+        return x

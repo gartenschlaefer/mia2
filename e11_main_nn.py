405ef6ebf8601a1e4c6c491f16ded79abd5a32dc
Nico Seddiki <nico.seddiki@gmail.com>
2020-06-28 20:05:01 +0200

    convert numpy arrays as pytorch tensors

--- e11_main_nn.py
+++ e11_main_nn.py
@@ -1,6 +1,6 @@
 # Third party modules:---------------------------------------------------------
+import torch 
 import numpy as np
-import torch as pyt
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -10,29 +10,26 @@ from mia2 import label_to_index
 
 if __name__ == "__main__":
     
-    # plot paths
+    # Part 1 - Loading---------------------------------------------------------
+    
     plot_path = 'ignore/ass10_data/plots/'
-
-    # load data
     data = loadmat( './ignore/ass11_data/BspDrums.mat' )
 
-    # get data arrays x:[n x m] n samples, m features
-    X  = data[ 'drumFeatures' ][0][0][0]
+    X  = data[ 'drumFeatures' ][0][0][0] 
+    y  = data[ 'drumFeatures' ][0][0][1] 
     
-    # get the data labels
-    y  = data[ 'drumFeatures' ][0][0][1]
-    
-    # get shape of things
-    n, m = X.shape  
+    n , m = X.shape  
     
     # get labels
     labels = np.unique( y )
-    
-    # tranfer labels to int indices
     y = label_to_index( y, labels )
-    print( y )
     
     # print some info
     print("num samples: {}, num features: {}, labels: {}".
         format( n, m, labels ) )
-    
+
+    # Part 2 - Convert to Torch tensors:---------------------------------------
+    torch.set_default_dtype( torch.float64 )
+
+    X_tensor = torch.from_numpy( X )
+    y_tensor = torch.from_numpy( y )
